9744a3d25e187d930b5e9ae2b8b75c85f2a3f725
harsh <harrypotter8401@gmail.com>
2022-08-26 20:14:15 +0530

    completedTill07

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr,...theArgs) {
+    for(const arg of theArgs){
+        for(let i=0;i<arr.length;i++){
+            if(arg===arr[i]){
+                arr.splice(i,1);
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fah) {
+  let cel=(fah-32)*(5/9);
+  return Math.round(cel*10)/10;
 };
 
-const ctof = function() {
-
+const ctof = function(cel) {
+  let fh=cel*(9/5)+32;
+  return Math.round(fh*10)/10;
 };
 
 // Do not edit below this line
